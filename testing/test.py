176d40a25dcc2baad729c2d10d75e0aecb2e9cd4
m-root <toyota100>
2020-04-17 13:47:26 +0300

    Update on the test file

--- testing/test.py
+++ testing/test.py
@@ -1,125 +1,143 @@
 from primebit.primebit import PrimebitPrivate, PrimebitPublic
 
+
 public = PrimebitPublic(type='live')
 
-# Details on crypto currencies available on the exchange
+''' Details on crypto currencies available on the exchange'''
 
 data = public.assets()
 print(data)
 
-# Details on crypto currencies available on the exchange
+''' Details on crypto currencies available on the exchange'''
 
 data = public.assets()
 print(data)
 
-# Get symbols
+''' Get symbols'''
 
 data = public.getsymbols()
 print(data)
 
-# Get symbol daily stats
+''' Get symbol daily stats'''
 
 
 data = public.symbolsDailyStats(symbol='BTCUSD')
 print(data)
 
-# Get aggregated 32-levels of order book
+''' Get aggregated 32-levels of order book'''
 
 data = public.orderBook(symbol='BTCUSD')
 print(data)
 
-# Overview of market data for all tickers
+''' Overview of market data for all tickers'''
 
 data = public.getAllTicker()
 print(data)
 
-# Get data for a specific ticker
+''' Get data for a specific ticker'''
 
 data = public.getTicker(symbol='BTCUSD')
 print(data)
 
-# 24-hour rolling window price change statistics for all markets
+''' 24-hour rolling window price change statistics for all markets'''
 
 data = public._tickersStats()
 print(data)
 
-# 24-hour rolling window price change statistics for a specific pair
+''' 24-hour rolling window price change statistics for a specific pair'''
 
 data = public.getTickerStat(symbol='BTCUSD')
 print(data)
 
-# Trades completed in the last 24 hours for a given market.
+''' Trades completed in the last 24 hours for a given market.'''
 
 data = public.getMarketTrades(symbol='BTCUSD')
 print(data)
 
-# Trades completed in the last 10 minutes
+''' Trades completed in the last 10 minutes'''
 
 data = public.marketTradeCompleted(symbol='BTCUSD')
 print(data)
 
-################################################################################################3
+# ################################################################################################3
 
-# Get all user accounts
+''' # Get all user accounts'''
 
-Key = 'bb7b0ee4-77b7-4890-a812-3a40bd31727c'
-Secret = 'WnrKVFL5Dvom/U+1OAZ+bB0QVBET/LNIaFSLYoiw/VY='
-accountID = '418a622b-7b3e-466e-9ee5-b79c7cf09a37'
+
+Key = '6482d965-71e8-4d38-bec0-e2c72325835b'
+Secret = 'o60jcowlM19wHQsmHoIG9sAMbRYWPP5a53CfaW5VeBg='
+accountID = '510903ae-57ee-44b8-a1c8-a763d4eef930'
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 
 data = private.getAllAccounts()
-print(data)
+for d in data:
+    print(d)
 
-# Get account
+'''Get account'''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.getAccount()
 print(data)
 
-# List account positions
+''' List account positions'''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.getAccountPositions()
 print(data)
 
-# Get a particular position
+''' Get a particular position '''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.getPosition(symbol='BTCUSD')
 print(data)
 
-# Buy at market with a market order at current price
+''' Buy at market with a market order at current price '''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
-data = private.marketBuy(symbol='BTCUSD', volume='0.001', comment='', stopPrice='')
+data = private.marketBuy(symbol='BTCUSD', volume='0.001')
 print(data)
 
-# Sell at market with a market order at current price
+''' Sell at market with a market order at current price '''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.marketSell(symbol='BTCUSD', volume='0.001', comment='', stopPrice='')
 print(data)
 
-# Buy Limit for an order
+''' Buy Limit for an order '''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
-data = private.buyLimit(symbol='BTCUSD', volume='0.001', price='7000', comment='', stopPrice='')
+data = private.buyLimit(symbol='ETHUSD', volume='0.001', price='7000')
 print(data)
 
-# Sell Limit for an order
+''' Sell Limit for an order'''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.sellLimit(symbol='BTCUSD', volume='0.001', price='7000', comment='', stopPrice='')
 print(data)
 
-# Cancel current pending orders
+''' Cancel current pending orders '''
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.deleteOrder(order_id=135153)
 print(data)
 
-# Listing all orders
+''' Listing all orders'''
+
 
 private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
 data = private.getAccountOrders()
+print(data)
+
+
+private = PrimebitPrivate(accountID=accountID, key=Key, secret=Secret)
+data = private.createOrder(
+    volume='0.005',
+    type="limit",
+    symbol="BTCUSDT",
+    stop_price="0.0",
+    side="buy",
+    limit_price="7000.7",
+    fill_type="immediate-or-cancel",
+    comment=""
+)
 print(data)
